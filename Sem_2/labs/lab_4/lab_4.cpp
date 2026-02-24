33919e811faf1200f6e16d811b0ea25d313772d6
Egovcev Ivan <egovcevivan@mail.ru>
2026-02-24 16:37:23 +0500

    Fixed lab 1

--- Sem_2/labs/lab_4/lab_4.cpp
+++ Sem_2/labs/lab_4/lab_4.cpp
@@ -1,50 +1,62 @@
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
+
+#define Size 100
 
 using namespace std;
 
 int main() {
     srand(time(0));
 
-    const int Size = 10;
     int arr[Size];
-    int arr_2[Size - 1];
-    int arr_3[Size];
+    int real_size;
     int range_max = 100;
 
-    for (int i = 0; i < Size; i++) {
+    cout << "Enter a size of list: ";
+    cin >> real_size;
+
+    for (int i = 0; i < real_size; i++) {
         arr[i] = ((double)rand() / RAND_MAX) * range_max;
         cout << arr[i] << " ";
     }
     cout << endl;
 
-    int k, j = 0;
+    int k, i = 0;
+    bool is_del = false;
 
     cout << "Enter k: ";
     cin >> k; 
     
-    for (int i = 0; i < Size; i++) {
-        if ((i + 1) != k) {
-            arr_2[j++] = arr[i];
+    while (i < real_size) {
+        if ((i + 1) == k) {
+            is_del = true;
         }
-    }
-
-    j = 0;
-    int i = 0;
-    bool is_inserted = false;
-
-    while (i < Size) {
-        arr_3[j++] = arr_2[i];
-        if ((arr_2[i] % 2 == 0) && !is_inserted) {
-            arr_3[j++] = arr_2[i - 1] + 2;
-            is_inserted = true;
+        if (is_del == true) {
+            arr[i] = arr[i + 1];
         }
         i++;
     }
 
-    for (int i = 0; i < Size; i++) {
-        cout << arr_3[i] << " ";
+    int temp1, temp2;
+    bool is_inserted = false;
+    i = 0;
+
+    while (i < real_size) {
+        if (arr[i] % 2 == 0 && is_inserted == false) {
+            temp1 = arr[i + 1];
+            arr[i + 1] = arr[i - 1] + 2;
+            is_inserted = true;
+        }
+        else if (is_inserted == true) {
+            temp2 = arr[i + 1];
+            arr[i + 1] = temp1;
+            temp1 = temp2;
+        }
+        i++; 
+    }
+
+    for (int i = 0; i < real_size; i++) {
+        cout << arr[i] << " ";
     }
 
     cout << endl;
