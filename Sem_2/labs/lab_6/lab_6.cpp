ea678b11f4f562f307113ca00ff0b344beb88ae7
Egovcev Ivan <egovcevivan@mail.ru>
2026-02-26 17:10:56 +0500

    Update lab_6

--- Sem_2/labs/lab_6/lab_6.cpp
+++ Sem_2/labs/lab_6/lab_6.cpp
@@ -7,7 +7,7 @@ int main() {
 
     for (int i = 0; i < s.size() - 1; i++) {
         for (int j = 0; j < s.size() - 1; j++) {
-            if (isdigit(s[j]) && !isdigit(s[j + 1])) {
+            if ((48 <= s[j] && s[j] <= 57) && (s[j + 1] < 48 || s[j + 1] > 57)) {
                 char temp = s[j];
                 s[j] = s[j + 1];
                 s[j + 1] = temp;
